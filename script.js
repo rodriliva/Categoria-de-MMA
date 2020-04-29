7754b5d1cfacfff5570c107885aa832cff051732
rodrigoliva <rodrigolivavelho@gmail.com>
2020-04-29 02:06:47 -0300

    Botões masculino e feminino com a função nova integrada close #3 #4

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
-const changeValue = () => {
-var teste = document.getElementById("fname").value;  
-function kilostoPounds(teste) { return ((teste * 2.2046).toFixed()) }
-const finalPound = kilostoPounds(teste);
-
-
-switch (true) {
-    case finalPound == 0:
-        document.getElementById("resultPound").innerHTML = `Coloque seu peso e vamos transformar em libras para descobrir em que categoria do UFC você lutaria!`;
-        break;
-    case finalPound <= 123:
-        document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Mosca</b>, pois você tem o que equivale a ${finalPound}lb`;
-        break;
-    case finalPound <= 135:
-        document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Galo</b>, pois você tem o que equivale a ${finalPound}lb`;
-        break;
-    case finalPound <= 145:
-        document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Pena</b>, pois você tem o que equivale a ${finalPound}lb`;
-        break;
-    case finalPound <= 155:
-        document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Leve</b>, pois você tem o que equivale a ${finalPound}lb`;
-        break;
-    case finalPound <= 171:
-        document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Meio Medio</b>, pois você tem o que equivale a ${finalPound}lb`;
-        break;
-    case finalPound <= 185:
-        document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Medio</b>, pois você tem o que equivale a ${finalPound}lb`;
-        break;
-    case finalPound <= 205:
-        document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Meio Pesado</b>, pois você tem o que equivale a ${finalPound}lb`;
-        break;
-    case finalPound <= 265:
-        document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Pesado</b>, pois você tem o que equivale a ${finalPound}lb`;
-        break;
-    default: document.getElementById("resultPound").innerHTML = "Digite um peso correto";
-}
+function calc() {
+    if (document.getElementById('male').checked) {
+        var teste = document.getElementById("fname").value;
+        function kilostoPounds(teste) { return ((teste * 2.2046).toFixed()) }
+        const finalPound = kilostoPounds(teste);
+        switch (true) {
+            case finalPound == 0:
+                document.getElementById("resultPound").innerHTML = `Coloque seu peso e vamos transformar em libras para descobrir em que categoria do UFC você lutaria!`;
+                break;
+            case finalPound <= 125:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Mosca</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound <= 135:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Galo</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound <= 145:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Pena</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound <= 155:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Leve</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound <= 171:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Meio Medio</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound <= 185:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Medio</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound <= 205:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Meio Pesado</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound >= 205:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Pesado</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            default: document.getElementById("resultPound").innerHTML = "Digite um peso correto";
+        };
+    } else {
+        var teste = document.getElementById("fname").value;
+        function kilostoPounds(teste) { return ((teste * 2.2046).toFixed()) }
+        const finalPound = kilostoPounds(teste);
+        switch (true) {
+            case finalPound == 0:
+                document.getElementById("resultPound").innerHTML = `Coloque seu peso e vamos transformar em libras para descobrir em que categoria do UFC você lutaria!`;
+                break;
+            case finalPound <= 115:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Palha</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound <= 125:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Mosca</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound <= 135:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Galo</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound <= 145:
+                document.getElementById("resultPound").innerHTML = `Você seria da categoria <b>Peso Pena</b>, pois você tem o que equivale a ${finalPound}lb`;
+                break;
+            case finalPound > 145:
+                document.getElementById("resultPound").innerHTML = `O limite das categorias femininas é 145lb e você possui ${finalPound}lb`;
+                break;
+            default: document.getElementById("resultPound").innerHTML = "Digite um peso correto";
+        };
+    }
 }
